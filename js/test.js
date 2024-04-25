3bdee89464aba118eaeddfbe140b19a600d30477
LinQuixote <leein9404@naver.com>
2024-04-25 19:24:49 +0900

    feat : 디자인

--- js/test.js
+++ js/test.js
@@ -1,18 +1,18 @@
-// const options = {
-//   method: "GET",
-//   headers: {
-//     accept: "application/json",
-//     Authorization:
-//       "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MTQ5MzRmNzg4ZjRlYzI1NmViY2RlYzcyNzVhMDdlZCIsInN1YiI6IjY2MjdhNGMyNjJmMzM1MDE2NGQ5ZDU0NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.UrmYmAFfVZN-YSlLPyTfP10QreSXWz1VYKRQeak1yxU",
-//   },
-// };
-// // encodeURIComponent
-// const langUrl = "https://api.themoviedb.org/3/configuration/languages";
-// const url = "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1";
-// export const data = fetch(langUrl, options)
-//   .then((response) => response.json())
-//   .then((response) => console.log(response))
-//   .catch((err) => console.error(err));
+const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MTQ5MzRmNzg4ZjRlYzI1NmViY2RlYzcyNzVhMDdlZCIsInN1YiI6IjY2MjdhNGMyNjJmMzM1MDE2NGQ5ZDU0NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.UrmYmAFfVZN-YSlLPyTfP10QreSXWz1VYKRQeak1yxU",
+  },
+};
+// encodeURIComponent
+const langUrl = "https://api.themoviedb.org/3/configuration/languages";
+const url = "https://api.themoviedb.org/3/movie/popular?language=ko&page=1";
+export const data = fetch(url, options)
+  .then((response) => response.json())
+  .then((response) => console.log(response))
+  .catch((err) => console.error(err));
 
 // t;
 
